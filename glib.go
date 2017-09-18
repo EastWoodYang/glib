88638b7dc5073daeac63d154763ee0c71beb218c
liuming <liuming@chinawlx.com>
2017-09-18 08:55:15 +0800

    update

--- glib.go
+++ glib.go
@@ -265,23 +265,23 @@ func StringSliceToString(stringSlice []string, args ...string) string {
 }
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * uint64差集
+ * 过滤uint64数组（从all中过滤所有other中的数据，返回未被过滤的数据集合）
  * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-func GetUint64DiffSet(all, other []uint64) []uint64 {
-	allMaps := make(map[uint64]bool, 0)
+func FilterUint64Slice(all, other []uint64) []uint64 {
+	allMap := make(map[uint64]bool, 0)
 	diffSet := make([]uint64, 0)
 
 	for _, a_v := range all {
-		allMaps[a_v] = true
+		allMap[a_v] = true
 	}
 
 	for _, a_v := range other {
-		if _, isOk := allMaps[a_v]; isOk {
-			allMaps[a_v] = false
+		if _, isOk := allMap[a_v]; isOk {
+			allMap[a_v] = false
 		}
 	}
 
-	for k, v := range allMaps {
+	for k, v := range allMap {
 		if v {
 			diffSet = append(diffSet, k)
 		}
@@ -291,9 +291,9 @@ func GetUint64DiffSet(all, other []uint64) []uint64 {
 }
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * string差集
+ * 过滤字符数组（从all中过滤所有other中的数据，返回未被过滤的数据集合）
  * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-func GetStringDiffSet(all, other []string) []string {
+func FilterStringSlice(all, other []string) []string {
 	allMaps := make(map[string]bool, 0)
 	diffSet := make([]string, 0)
 
@@ -314,6 +314,164 @@ func GetStringDiffSet(all, other []string) []string {
 	}
 
 	return diffSet
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 字符集合的交集合
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func StringInter(one, two []string) []string {
+	allMap := make(map[string]bool, 0)
+	interSet := make([]string, 0)
+
+	if len(one) == 0 && len(two) == 0 {
+		return interSet
+	}
+
+	if len(one) == 0 {
+		for _, v := range two {
+			allMap[v] = true
+		}
+	} else if len(two) == 0 {
+		for _, v := range one {
+			allMap[v] = true
+		}
+	} else {
+		for _, v := range one {
+			allMap[v] = true
+		}
+		for _, v := range two {
+			if _, isOk := allMap[v]; isOk {
+				allMap[v] = false
+			}
+		}
+	}
+
+	for k, v := range allMap {
+		if !v {
+			interSet = append(interSet, k)
+		}
+	}
+
+	return interSet
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 字符集合的并集合
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func StringUnion(one, two []string) []string {
+	allMap := make(map[string]string, 0)
+	union := make([]string, 0)
+
+	for _, v := range one {
+		allMap[v] = v
+	}
+
+	for _, v := range two {
+		if _, isOk := allMap[v]; !isOk {
+			allMap[v] = v
+		}
+	}
+
+	for _, v := range allMap {
+		union = append(union, v)
+	}
+
+	return union
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 字符集合的差集合
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func StringDiff(one, two []string) []string {
+	//并集合
+	union := StringUnion(one, two)
+
+	//交集合
+	inter := StringInter(one, two)
+
+	//差集合
+	diff := FilterStringSlice(union, inter)
+
+	return diff
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * uint64交集合
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func Uint64Inter(one, two []uint64) []uint64 {
+	allMap := make(map[uint64]bool, 0)
+	interSet := make([]uint64, 0)
+
+	if len(one) == 0 && len(two) == 0 {
+		return interSet
+	}
+
+	if len(one) == 0 {
+		for _, v := range two {
+			allMap[v] = true
+		}
+	} else if len(two) == 0 {
+		for _, v := range one {
+			allMap[v] = true
+		}
+	} else {
+		for _, v := range one {
+			allMap[v] = true
+		}
+		for _, v := range two {
+			if _, isOk := allMap[v]; isOk {
+				allMap[v] = false
+			}
+		}
+	}
+
+	for k, v := range allMap {
+		if !v {
+			interSet = append(interSet, k)
+		}
+	}
+
+	return interSet
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * uint64并集合
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func Uint64Union(one, two []uint64) []uint64 {
+	allMap := make(map[uint64]uint64, 0)
+	union := make([]uint64, 0)
+
+	for _, v := range one {
+		allMap[v] = v
+	}
+
+	for _, v := range two {
+		if _, isOk := allMap[v]; !isOk {
+			allMap[v] = v
+		}
+	}
+
+	for _, v := range allMap {
+		union = append(union, v)
+	}
+
+	return union
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * uint64差集合
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func Uint64Diff(one, two []uint64) []uint64 {
+	//并集合
+	union := Uint64Union(one, two)
+
+	//交集合
+	inter := Uint64Inter(one, two)
+
+	//差集合
+	diff := FilterUint64Slice(union, inter)
+
+	return diff
 }
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
