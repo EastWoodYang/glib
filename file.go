d8138186039f9dc5b1ac269be4792f0af51b0145
liuming <liuming@chinawlx.com>
2017-12-28 00:33:44 +0800

    update

--- file.go
+++ file.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -197,6 +198,49 @@ func Command(cmd string, args ...string) (string, error) {
 	command := exec.Command(cmd, args...)
 	output, err := command.Output()
 	return string(output), err
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 根据进程名称找到对应的pid
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func FindPid(processName string) (int, error) {
+	var pid int
+	args := []string{
+		"-ef",
+		fmt.Sprintf("|grep %s", processName),
+		"|grep -v grep",
+		"|grep -v PPID",
+		"|awk '{ print $2}'",
+	}
+
+	output, err := Command("ps", args...)
+	if output != "" {
+		output = strings.Replace(output, " ", "", -1)
+		output = strings.Replace(output, "\n", "", -1)
+		pid, err = strconv.Atoi(output)
+	}
+
+	return pid, err
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 运行指定的进程
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func StartProcess(processName string) (string, error) {
+	return Command("nohup", processName, "2>&1", "&")
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 杀死指定pid进程
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func StopProcess(processName string) (string, error) {
+	pid, err := FindPid(processName)
+
+	if err != nil {
+		return "", err
+	}
+
+	return Command("kill", "-SIGTERM", fmt.Sprintf("%d", pid))
 }
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- http.go
+++ http.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"mime/multipart"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -220,4 +221,26 @@ func UrlDecode(sourceUrl string) string {
 	}
 
 	return decodeUrl
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 判断是否本地Ip
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func IsLocalIp(ip string) (bool, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return false, err
+	}
+
+	for i := range addrs {
+		intf, _, err := net.ParseCIDR(addrs[i].String())
+		if err != nil {
+			return false, err
+		}
+
+		if net.ParseIP(ip).Equal(intf) {
+			return true, nil
+		}
+	}
+	return false, nil
 }
